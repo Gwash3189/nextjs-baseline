b572e05bb06039a3c03c0db89e8afc86c6ca4594
Adam <adamb3189@gmail.com>
2022-02-10 16:56:24 +1100

    chore: lint fix

--- components/buttons/CTAButton.tsx
+++ components/buttons/CTAButton.tsx
@@ -9,8 +9,8 @@ type Props = {
 
 export default function CTAButton (props: Props) {
   return (
-    <button 
-      onClick={props.onClick} 
-      className={classnames("py-2 text-center font-semibold text-md bg-pink-400 hover:bg-pink-500 transition-all drop-shadow-sm hover:drop-shadow-md text-white rounded-lg ml-6", props.className)}>{props.children}</button>
+    <button
+      onClick={props.onClick}
+      className={classnames('py-2 text-center font-semibold text-md bg-pink-400 hover:bg-pink-500 transition-all drop-shadow-sm hover:drop-shadow-md text-white rounded-lg ml-6', props.className)}>{props.children}</button>
   )
 }
